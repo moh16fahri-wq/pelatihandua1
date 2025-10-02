997ac8702fdf589ce593a4db6749fe527de2c1e8
moh16fahri-wq <moh16fahri@gmail.com>
2025-10-02 14:01:50 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const tombolSapa = document.getElementById('sapaButton'):
+const tombolSapa = document.getElementById('sapaButton');
 tombolSapa.addEventListener('click', function(){
   alert('Hallo! Terimakasih sudah berkunjung');
 });
